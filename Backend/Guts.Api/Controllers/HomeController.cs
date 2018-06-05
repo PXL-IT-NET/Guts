7377876b665d502bab2cd1e5b81589c6d842e5d3
Wesley Hendrikx <wesleyhendrikx@hotmail.com>
2018-06-05 14:26:57 +0200

    Redirect start page to swagger doc page

---
+++ Backend/Guts.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guts.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return RedirectPermanent("~/docs");
+        }
+    }
+}
