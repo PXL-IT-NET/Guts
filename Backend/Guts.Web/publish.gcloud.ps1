e52e744be87288193cf90a0f2bbd15dcb64d773a
Wesley Hendrikx <wesleyhendrikx@hotmail.com>
2018-08-09 16:49:43 +0200

    finetune publication

---
+++ Backend/Guts.Web/publish.gcloud.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\Workspace\PXL-IT-NET\Guts\Backend\Guts.Web
+dotnet restore
+dotnet publish -c Release
+gcloud config set project guts-web
+gcloud app deploy .\bin\Release\netcoreapp2.1\publish\app.yaml
+Y
+gcloud app browse
