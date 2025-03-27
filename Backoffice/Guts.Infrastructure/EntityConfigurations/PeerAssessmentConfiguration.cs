d62ed532da9716514253046b7e7f3e67589346c8
Wesley Hendrikx <wesleyhendrikx@hotmail.com>
2025-03-27 12:27:57 +0100

    add unique index on peerassessments
    Combination of ProjectTeamAssessmnetId, UserId and SubjectId must be unique. Now it is enforced in the DB

--- Backoffice/Guts.Infrastructure/EntityConfigurations/PeerAssessmentConfiguration.cs
+++ Backoffice/Guts.Infrastructure/EntityConfigurations/PeerAssessmentConfiguration.cs
@@ -11,15 +11,24 @@ internal class PeerAssessmentConfiguration : IEntityTypeConfiguration<PeerAssess
     {
         builder.ToTable("PeerAssessments");
 
+        string userIdForeignKeyName = nameof(PeerAssessment.User) + "Id";
+        string subjectIdForeignKeyName = nameof(PeerAssessment.Subject) + "Id";
+
         builder.HasOne(pa => pa.User)
             .WithMany()
-            .HasForeignKey(nameof(PeerAssessment.User) + "Id")
+            .HasForeignKey(userIdForeignKeyName)
             .OnDelete(DeleteBehavior.NoAction)
             .IsRequired();
-        builder.HasOne(pa => pa.Subject).WithMany().HasForeignKey(nameof(PeerAssessment.Subject) + "Id").IsRequired();
+        
+        builder.HasOne(pa => pa.Subject).WithMany().HasForeignKey(subjectIdForeignKeyName).IsRequired();
 
         builder.Property(pa => pa.CooperationScore).IsRequired().HasConversion(score => score.Value, scoreValue => new AssessmentScore(scoreValue));
         builder.Property(pa => pa.ContributionScore).IsRequired().HasConversion(score => score.Value, scoreValue => new AssessmentScore(scoreValue));
         builder.Property(pa => pa.EffortScore).IsRequired().HasConversion(score => score.Value, scoreValue => new AssessmentScore(scoreValue));
+
+        builder.HasIndex(
+                nameof(PeerAssessment.ProjectTeamAssessmentId), 
+                userIdForeignKeyName, 
+                subjectIdForeignKeyName).IsUnique();
     }
 }
---
+++ Backoffice/Guts.Infrastructure/Migrations/20250327111344_UniqueIndexOnPeerAssessment.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Guts.Infrastructure.Migrations
+{
+    public partial class UniqueIndexOnPeerAssessment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PeerAssessments_ProjectTeamAssessmentId",
+                table: "PeerAssessments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeerAssessments_ProjectTeamAssessmentId_UserId_SubjectId",
+                table: "PeerAssessments",
+                columns: new[] { "ProjectTeamAssessmentId", "UserId", "SubjectId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PeerAssessments_ProjectTeamAssessmentId_UserId_SubjectId",
+                table: "PeerAssessments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeerAssessments_ProjectTeamAssessmentId",
+                table: "PeerAssessments",
+                column: "ProjectTeamAssessmentId");
+        }
+    }
+}
